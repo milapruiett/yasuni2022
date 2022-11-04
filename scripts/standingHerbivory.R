7ed6aa7c192800ba80e1ddb4bb7e97a6eebc865e
milapruiett <ameliapruiett@lclark.edu>
2022-11-03 17:23:23 -0700

    making a clean version of the standing herbivory analyses

--- scripts/standingHerbivory.R
+++ scripts/standingHerbivory.R
@@ -1,10 +1,13 @@
-install.packages("gamlss")
+#install.packages("gamlss")
 
+# load dependencies
 library("tidyverse")
 library("gamlss")
 
+# read the data
 standing <- read_csv("data/StandingHerbivoryDataMila.xlsx - Sheet1.csv")
 
+# format existing columns
 standing$LeafWidth <- as.numeric(standing$LeafWidth)
 standing$LeafLength <- as.numeric(standing$LeafLength)
 standing$UH <- as.numeric(standing$UH)
@@ -14,51 +17,19 @@ standing <- standing %>% mutate(LeafArea = pi * LeafWidth / 2 * LeafLength / 2)
 
 # calculate total herbivory
 standing <- standing %>% mutate(totalHerb = UH + RH)
-standing <- standing %>% mutate(herbPerc = totalHerb / LeafArea)
 
 # calculate herbivory percentage
-standing <- standing %>% mutate(uhHerbPerc = UH / LeafArea)
-standing <- standing %>% mutate(rhHerbPerc = RH / LeafArea)
-
-# what percent of leaves have rolled herbivory
-ggplot(standing, aes(LeafNo, rhHerbPerc)) + 
-  geom_boxplot()  + 
-  scale_y_continuous(trans='log10')
-
-# yes unrolled herb or no
-standing <- standing %>% mutate(uhPresence = case_when(UH <= 0 ~ "none", UH > 0 ~ "some"))
-standing %>% group_by(Sp, uhPresence) %>% summarize(n=n())
+standing <- standing %>% mutate(uhHerbPerc = UH / LeafArea) %>% 
+  mutate(rhHerbPerc = RH / LeafArea) %>% 
+  mutate(herbPerc = totalHerb / LeafArea)
 
 # remove the NA and unknown species
 standing <- standing %>% filter(Sp == "giga"| Sp == "scaber"| Sp == "velutia"| Sp == "stricta")
 
-# now calculate the percentage
-105 / 215
-137 / (78 + 137)
-110 / (38 + 110)
-101 / (101 + 62)
-
-# rolled herb?
-standing <- standing %>% mutate(rhPresence = case_when(RH <= 0 ~ "none", RH > 0 ~ "some"))
-
-standing %>% group_by(Sp) %>% summarize(meanRolledHerbPerPlant = mean(rhHerbPerc, na.rm = T), 
-                                        meanUnrolledHerbPerPlant = mean(uhHerbPerc, na.rm = T), )
-
-# viz
-standing %>% group_by(Sp, ID) %>% ggplot(aes(y = uhHerbPerc, x = LeafNo)) + geom_boxplot() + scale_y_continuous(trans='log10')
-
-standing %>% ggplot(aes(y = uhHerbPerc, x = rhHerbPerc, color = LeafNo)) + geom_point() + facet_wrap(~ Sp)
-
-standing %>% ggplot(aes(x = rhHerbPerc)) + facet_wrap(~ Sp) + geom_histogram() # for sure zero infalted
-
-#logit(standing) %>% group_by(Sp, ID) %>% ggplot(aes(y = rhHerbPerc, x = LeafNo)) + 
-  #geom_boxplot() + 
- # facet_wrap(~ Sp) 
-
-#logit(standing$rhHerbPerc)
-
-# how common is herbivory ? 
-standing <- standing %>% mutate(herbPresence = case_when(totalHerb == 0 ~ 0, totalHerb > 0 ~ 1 ))
+# creating presence / absence columns for all types of herbivory
+standing <- standing %>% mutate(rhPresence = case_when(RH <= 0 ~ 0, RH > 0 ~ 1)) %>% 
+  mutate(herbPresence = case_when(totalHerb <= 0 ~ 0, totalHerb > 0 ~ 1 )) %>% 
+  standing <- standing %>% mutate(uhPresence = case_when(totalHerb <= 0 ~ 0, totalHerb > 0 ~ 1 ))
 
 # on a given plant, how many leaves will have herbivory on average?
 herbByPlant <- standing %>% 
@@ -95,51 +66,52 @@ ggplot(standing, aes(Sp, herbPerc, fill =Sp)) +
 
 # does the amount of herbivory change depending on leaf age- is that different for rolled vs unrolled?
 
+# create long form data, where each row is a percent of herbivory
 herbivoryLongForm <- standing %>% 
-  select(c("Sp", "ID", "LeafNo", "uhHerbPerc", "rhHerbPerc")) %>% 
+  dplyr::select("Sp", "ID", "LeafNo", "uhHerbPerc", "rhHerbPerc") %>% 
   pivot_longer(cols = c(uhHerbPerc, rhHerbPerc), 
                names_to = "Rolled",
                values_to = "percentHerb")
 
 herbivoryLongForm %>% 
-  subset(LeafNo != "Terminal" & Sp == "giga") %>% 
+  subset(LeafNo != "Terminal") %>% 
   ggplot(aes(LeafNo, percentHerb, fill = Rolled)) +
   geom_boxplot() + 
   scale_fill_discrete(name = "Roll Status", labels =c("Rolled", "Unrolled")) +
   xlab("Leaf Number") +
   ylab("Percent Herbivory") +
-  scale_y_continuous(trans = "pseudo_log")
- # i'm not sure if boxplot or scatterplot is correct
+  scale_y_continuous(trans = "pseudo_log") +
+  facet_wrap(~Sp)
 
-library("gamlss")
+# i'm not sure if boxplot or scatterplot is correct
 
+# create a data set for beta regression (remove the one where the percent is greater than 1)
+dataForBetaRegStandingHerb <- herbivoryLongForm %>% filter(percentHerb < 1) %>% na.omit()
 
+# count to see how zero-inflated it is
 dataForBetaRegStandingHerb %>% mutate(dataClass = case_when(percentHerb == 1 ~ "one",
-                                           percentHerb == 0 ~ "zero",
-                                            percentHerb != 0 & percentHerb != 1 ~ "between")) %>% 
+                                                            percentHerb == 0 ~ "zero",
+                                                            percentHerb != 0 & percentHerb != 1 ~ "between")) %>% 
   group_by(dataClass) %>% 
   summarize(n())
 
-dataForBetaRegStandingHerb <- herbivoryLongForm %>% filter(percentHerb < 1) %>% na.omit()
-m6 <- gamlss(percentHerb ~ LeafNo + Sp + Rolled,  family = BEZI, data = dataForBetaRegStandingHerb, trace = F)
-summary(m6)   
+# run the zero-inflated beta regression, for each species
+modHVel <- gamlss(percentHerb ~ LeafNo + Rolled,  family = BEZI, data = dataForBetaRegStandingHerb[dataForBetaRegStandingHerb$Sp == "velutia" , ], trace = F)
+summary(modHVel)   
 
-summary(aov(data = dataForBetaRegStandingHerb, percentHerb ~ LeafNo + Sp + Rolled )) # yeah idk how to interpret the results
+modHStr <- gamlss(percentHerb ~ LeafNo + Rolled,  family = BEZI, data = dataForBetaRegStandingHerb, trace = F)
+summary(modHStr)   
 
+modCSca <- gamlss(percentHerb ~ LeafNo +  Rolled,  family = BEZI, data = dataForBetaRegStandingHerb, trace = F)
+summary(modCSca)   
 
-# i feel this viz isn't the most effective
+modRgig <- gamlss(percentHerb ~ LeafNo + Rolled,  family = BEZI, data = dataForBetaRegStandingHerb, trace = F)
+summary(modRgig)   
+
+# a viz with the summary
 longformSummary <- herbivoryLongForm %>% 
   group_by(Sp, Rolled, LeafNo) %>% 
   summarize(meanHerb = mean(percentHerb, na.rm = T), sdHerb = sd(percentHerb, na.rm = T))
-  
-longformSummary %>% 
-  ggplot(aes(LeafNo, meanHerb, fill = Rolled)) +
-  geom_bar(stat = "identity") +
-  facet_wrap(~Sp) +
-  geom_errorbar(aes(ymin = meanHerb - sdHerb, ymax = meanHerb + sdHerb)) 
-
-# I think the proper regression for this will be zero inflated beta
-
 
 Sp.labs <- c("R. gigante", "C. scaber", "H. scaber", "H. velutina")
 names(Sp.labs) <- c("giga", "scaber","stricta",  "velutia")
@@ -156,8 +128,6 @@ longformSummary %>%
              labeller = labeller(Sp = Sp.labs)) +
   theme(strip.text.x = element_text(face = "italic")) +
   ylab("Mean Percent Herbivory") 
+# can add error bars with:  geom_errorbar(aes(ymin = meanHerb - sdHerb, ymax = meanHerb + sdHerb), position = position_dodge(width = 0.9))
 
 
-herbivoryLongForm %>% 
-  ggplot(aes(x = LeafNo, y = percentHerb )) +
-  geom_point()
